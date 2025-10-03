bd4acecf9faca011d1bd3539fa290ee9c2dccc8c
Thiha Ye Yint Aung <thihayeyintaung110919@gmail.com>
2025-10-03 10:45:16 -0500

    LazyGrids

---
+++ MyAppLesson4/app/src/main/java/com/miu/myapp_lesson4/LazyHorizontalGridScreen.kt
@@ -0,0 +1,46 @@
+package com.miu.myapp_lesson4
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.miu.myapp_lesson4.data.DataSource
+
+@Composable
+fun LazyHorizontalGridScreen(modifier: Modifier = Modifier) {
+    Scaffold { innerPadding ->
+        LazyHorizontalGrid(
+            rows = GridCells.Fixed(2),
+            modifier = modifier
+                .padding(innerPadding)
+        ) {
+            items(DataSource.loadData()) {
+                Image(
+                    painter = painterResource(id = it.image),
+                    contentDescription = stringResource(id = it.title),
+                    modifier = Modifier
+                        .aspectRatio(2f),
+                    contentScale = ContentScale.Crop
+                )
+            }
+        }
+    }
+}
+
+@Preview(
+    showSystemUi = true
+)
+@Composable
+private fun LazyHorizontalGridScreenPreview() {
+    LazyHorizontalGridScreen()
+}
