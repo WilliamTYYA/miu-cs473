43be7e0694e1016feab004ec6948c77cb593a9ca
Thiha Ye Yint Aung <thihayeyintaung110919@gmail.com>
2025-10-10 16:21:11 -0500

    bottom bar nav

--- NavigationApp/app/src/main/java/com/miu/navigationapp/nav/AppNavGraph.kt
+++ NavigationApp/app/src/main/java/com/miu/navigationapp/nav/AppNavGraph.kt
@@ -1,5 +1,15 @@
 package com.miu.navigationapp.nav
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -18,24 +28,67 @@ import com.miu.navigationapp.feature.setting.SettingsScreen
 
 @Composable
 fun AppNavGraph(modifier: Modifier = Modifier) {
+
     // create a back stack object
     val backStack = rememberNavBackStack<AppNavKey>(Home)
 
-    fun navigateToProductList(category: Category) {
-        backStack.add(ProductList(category))
-    }
+    Scaffold(
+        bottomBar = {
+            NavigationBar {
+                NavigationBarItem(
+                    label = { Text("Home") },
+                    selected = backStack.lastOrNull() == Home,
+                    onClick = {
+                        backStack.clear()
+                        backStack.add(Home)
+                    },
+                    icon = {
+                        BadgedBox(
+                            badge = {
+                                if (backStack.lastOrNull() == ProductList(Category.ELECTRONIC)) {
+                                    Badge {
+                                        val badgeNumber = "999+"
+                                        Text(
+                                            badgeNumber
+                                        )
+                                    }
+                                }
+                            }
+                        ) {
+                            Icon(Icons.Default.Home, contentDescription = "Home")
+                        }
+                    }
+                )
+                NavigationBarItem(
+                    label = { Text("Settings") },
+                    selected = backStack.lastOrNull() == Settings,
+                    onClick = {
+                        backStack.clear()
+                        backStack.add(Settings)
+                    },
+                    icon = {
+                        Icon(Icons.Default.Settings, contentDescription = "Home")
+                    }
+                )
+            }
+        }
+    ) { innerPadding ->
 
-    fun navigateToProductDetail(id: String) {
-        backStack.add(ProductDetail(id))
-    }
+        fun navigateToProductList(category: Category) {
+            backStack.add(ProductList(category))
+        }
 
-    fun navigateToSetting() {
-        backStack.add(Settings)
-    }
+        fun navigateToProductDetail(id: String) {
+            backStack.add(ProductDetail(id))
+        }
 
-    NavDisplay(
-        backStack = backStack,
-        onBack  = {backStack.removeLastOrNull()},
+        fun navigateToSetting() {
+            backStack.add(Settings)
+        }
+
+        NavDisplay(
+            backStack = backStack,
+            onBack  = {backStack.removeLastOrNull()},
 //        entryProvider = { key ->
 //            when(key) {
 //                is Home -> NavEntry(Home) {
@@ -53,39 +106,41 @@ fun AppNavGraph(modifier: Modifier = Modifier) {
 //                else -> error("Unknown key: $key")
 //            }
 //        },
-        entryDecorators = listOf(
-            rememberSavedStateNavEntryDecorator(),
-            rememberViewModelStoreNavEntryDecorator()
-        ),
-        entryProvider = entryProvider {
-            entry<Home> {
-                HomeScreen(
-                    navigateToProductList = ::navigateToProductList,
-                    navigateToSettings = {
-                        backStack.add(Settings)
-                    },
-                    modifier = modifier
-                )
-            }
-            entry<ProductList> { productList: ProductList ->
-                val category: Category = productList.category
-                ProductListScreen(
-                    category = category,
-                    navigateToProductDetail = ::navigateToProductDetail,
-                    modifier = modifier
-                )
-            }
-            entry<ProductDetail> { productDetail: ProductDetail ->
-                var id: String = productDetail.productId
-                ProductDetailScreen(
-                    id,
-                    modifier
-                )
-            }
-            entry<Settings> {
-                SettingsScreen(modifier)
-            }
-        },
-        modifier = modifier
-    )
+            entryDecorators = listOf(
+                rememberSavedStateNavEntryDecorator(),
+                rememberViewModelStoreNavEntryDecorator()
+            ),
+            entryProvider = entryProvider {
+                entry<Home> {
+                    HomeScreen(
+                        navigateToProductList = ::navigateToProductList,
+                        navigateToSettings = {
+                            backStack.add(Settings)
+                        },
+                        modifier = modifier
+                    )
+                }
+                entry<ProductList> { productList: ProductList ->
+                    val category: Category = productList.category
+                    ProductListScreen(
+                        category = category,
+                        navigateToProductDetail = ::navigateToProductDetail,
+                        modifier = modifier
+                    )
+                }
+                entry<ProductDetail> { productDetail: ProductDetail ->
+                    var id: String = productDetail.productId
+                    ProductDetailScreen(
+                        id,
+                        modifier
+                    )
+                }
+                entry<Settings> {
+                    SettingsScreen(modifier)
+                }
+            },
+            modifier = modifier
+                .padding(innerPadding)
+        )
+    }
 }
